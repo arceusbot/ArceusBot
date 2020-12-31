f98745bf824ff192d1ee10789f8011071fb350e7
ARCEUS <76394775+arceusbot@users.noreply.github.com>
2020-12-31 17:22:42 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -49,7 +49,7 @@ for name in files:
 
 import userbot._core
 
-print("Aree On Ho Gaya Bc!! Abb jake .alive likh ke dekho!!.... Join @ArceusTheuserBot for any help..")
+print("Aree On Ho Gaya Bc!! Abb jake .alive likh ke dekho!!.... Join @ArceususerBot for any help..")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
